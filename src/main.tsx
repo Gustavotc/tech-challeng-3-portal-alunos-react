af43c92e8871f823ff989953a04d9dba03b36ec4
Gustavotc <gustavotch@hotmail.com>
2024-09-11 23:18:23 -0300

    chore: setup do chakra-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import GlobalStyle from "./globalStyle.ts";
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <GlobalStyle />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
