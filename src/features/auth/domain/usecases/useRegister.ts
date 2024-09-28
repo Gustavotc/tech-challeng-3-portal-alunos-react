0d7d31a36065fc1118979b7d4133ce3b1d926314
Vinícius Herrera da Silva <vini.hsilva@hotmail.com>
2024-09-27 22:05:27 -0300

    feat: adjust register screen

--- src/features/auth/domain/usecases/useRegister.ts
+++ src/features/auth/domain/usecases/useRegister.ts
@@ -1,15 +1,16 @@
 import { AuthRepository } from "../../infra/repositories/AuthRepository";
 import { IAuthRegisterUser } from "../interfaces/IAuthRegisterUser";
+import { IUser } from "../interfaces/IUser";
 
 interface IRegister {
-  register: (registerUser: IAuthRegisterUser) => Promise<void>; // async function to register a user
+  register: (registerUser: IAuthRegisterUser) => Promise<IUser>; // async function to register a user
 }
 
 export const useRegister = (): IRegister => {
   const authRepo = new AuthRepository();
 
-  const register = async (registerUser: IAuthRegisterUser) => {
-    await authRepo.register(registerUser);
+  const register = async (registerUser: IAuthRegisterUser): Promise<IUser> => {
+    return authRepo.register(registerUser);
   };
 
   return { register };
